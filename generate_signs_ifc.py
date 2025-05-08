e2735190a4478db12f9266478de8857dd252d220
Samuel <92334211+SmithSamP@users.noreply.github.com>
2025-05-08 09:41:34 -0700

    Added geographic placement

--- generate_signs_ifc.py
+++ generate_signs_ifc.py
@@ -4,7 +4,7 @@ import ifcopenshell.api.unit
 import ifcopenshell.api.context
 import ifcopenshell.api.project
 import ifcopenshell.api.geometry
-
+import helpers
 import numpy as np
 
 model = ifcopenshell.api.project.create_file()
@@ -19,41 +19,11 @@ context = ifcopenshell.api.context.add_context(model, context_type="Model")
 body = ifcopenshell.api.context.add_context(model, context_type="Model",
     context_identifier="Body", target_view="MODEL_VIEW", parent=context)
 
-def add_property_set(file, product):
-    pset = ifcopenshell.api.pset.add_pset(file, product=product, name="Signs")
-    ifcopenshell.api.pset.edit_pset(file, pset=pset, properties={"sign_int": 37})
+data = helpers.import_signs(r"data\Finalsets_merged_XYZ.csv", "US_SURVEY_FOOT")
 
-def create_sign(file, x, y, z):
-    matrix = np.array([[0, -1, 0, x], 
-                   [1, 0, 0, y], 
-                   [0, 0, 1, z],
-                   [0, 0, 0, 1]])
-    sign = ifcopenshell.api.root.create_entity(file, ifc_class = "IfcGeographicElement", predefined_type="Sign")
-    ifcopenshell.api.geometry.edit_object_placement(file, product=sign, matrix=matrix,is_si=True)
-
-    vertices = [[(-1.,-1.,0.), (-1.,1.,0.), (1.,1.,0.), (1.,-1.,0.), (0.,0.,1.)]]
-    faces = [[(0,1,2,3), (0,4,1), (1,4,2), (2,4,3), (3,4,0)]]
-    sign_representation = ifcopenshell.api.geometry.add_mesh_representation(file, context=body, vertices=vertices, faces=faces)
-    ifcopenshell.api.geometry.assign_representation(file,product=sign,representation=sign_representation)
-    add_property_set(model, sign)
-
-def create_point(file, x, y, z):
-    matrix = np.array([[0, -1, 0, x], 
-                   [1, 0, 0, y], 
-                   [0, 0, 1, z],
-                   [0, 0, 0, 1]])
-    sign = ifcopenshell.api.root.create_entity(file, ifc_class = "IfcAnnotation", predefined_type="SurveyPoint")
-    ifcopenshell.api.geometry.edit_object_placement(file, product=sign, matrix=matrix,is_si=True)
-
-    # vertices = [[(-1.,-1.,0.), (-1.,1.,0.), (1.,1.,0.), (1.,-1.,0.), (0.,0.,0.)]]
-    # faces = [[(0,1,2,3), (0,4,1), (1,4,2), (2,4,3), (3,4,0)]]
-    # sign_representation = ifcopenshell.api.geometry.add_mesh_representation(file, context=body, vertices=vertices, faces=faces)
-    # ifcopenshell.api.geometry.assign_representation(file,product=sign,representation=sign_representation)
-    add_property_set(model, sign)
+# create_point(model, -3, -3, 0)
+for index, row in data.iterrows():
+    helpers.create_point(model, body, row['X_2011NAD83'], row['Y_2011NAD83'], row['Z'])
 
 
-create_point(model, -3, -3, 0)
-create_sign(model, 3, 3, 0)
-
-
-model.write("ifc_out/element_model.ifc")
+model.write("ifc_out/points_signs.ifc")
---
+++ helpers.py
@@ -0,0 +1,57 @@
+import ifcopenshell.api.pset
+import ifcopenshell.api.root
+import ifcopenshell.api.unit
+import ifcopenshell.api.context
+import ifcopenshell.api.project
+import ifcopenshell.api.geometry
+import numpy as np
+import pandas as pd
+
+def add_property_set(file, product):
+    pset = ifcopenshell.api.pset.add_pset(file, product=product, name="Signs")
+    ifcopenshell.api.pset.edit_pset(file, pset=pset, properties={"sign_int": 37})
+
+def create_sign(file, body, x, y, z):
+    matrix = np.array([[0, -1, 0, x], 
+                   [1, 0, 0, y], 
+                   [0, 0, 1, z],
+                   [0, 0, 0, 1]])
+    sign = ifcopenshell.api.root.create_entity(file, ifc_class = "IfcGeographicElement", predefined_type="Sign")
+    ifcopenshell.api.geometry.edit_object_placement(file, product=sign, matrix=matrix,is_si=True)
+
+    vertices = [[(-1.,-1.,0.), (-1.,1.,0.), (1.,1.,0.), (1.,-1.,0.), (0.,0.,1.)]]
+    faces = [[(0,1,2,3), (0,4,1), (1,4,2), (2,4,3), (3,4,0)]]
+    sign_representation = ifcopenshell.api.geometry.add_mesh_representation(file, context=body, vertices=vertices, faces=faces)
+    ifcopenshell.api.geometry.assign_representation(file,product=sign,representation=sign_representation)
+    add_property_set(file, sign)
+
+def create_point(file, body, x, y, z):
+    matrix = np.array([[0, -1, 0, x], 
+                   [1, 0, 0, y], 
+                   [0, 0, 1, z],
+                   [0, 0, 0, 1]])
+    sign = ifcopenshell.api.root.create_entity(file, ifc_class = "IfcAnnotation", predefined_type="SurveyPoint")
+    ifcopenshell.api.geometry.edit_object_placement(file, product=sign, matrix=matrix,is_si=True)
+
+    # vertices = [[(0.,0.,0.), (0.,0.,0.), (0.,0.,0.), (0.,0.,0.), (0.,0.,0.)]]
+    # faces = [[(0,1,2,3), (0,4,1), (1,4,2), (2,4,3), (3,4,0)]]
+    # sign_representation = ifcopenshell.api.geometry.add_mesh_representation(file, context=body, vertices=vertices, faces=faces)
+    # ifcopenshell.api.geometry.assign_representation(file,product=sign,representation=sign_representation)
+    add_property_set(file, sign)
+
+def import_signs(csv_in, unit):
+    df = pd.read_csv(csv_in)
+    unit_dict = {
+        "US_SURVEY_FOOT": 1200/3937,
+        "INTERNATIONAL_FOOT" : 0.3048,
+        "METER" : 1
+    }
+    df['X_2011NAD83'] = df['X_2011NAD83']/unit_dict[unit]
+    df['Y_2011NAD83'] = df['Y_2011NAD83']/unit_dict[unit]
+    df['Z'] = df['Z']/unit_dict[unit]
+    return df
+
+
+if __name__ == "__main__":
+    import_signs(r"data\Finalsets_merged_XYZ.csv", "US_SURVEY_FOOT")
+
